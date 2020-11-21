f2719f0bb1a47dfd9212612097d443e598236917
nguthemay365 <72782795+nguthemay365@users.noreply.github.com>
2020-11-21 20:38:25 +0700

    Add files via upload

---
+++ Bài tập/bai7/admin/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container-fluid mt-3">
+         <h4 class="mb-2">Complex Form</h4>
+         <form>
+            <div class="form-row">
+               <div class="form-group col-sm-6">
+                  <label for="myEmail">Email</label>
+                  <input type="email" name="email" class="form-control" id="myEmail" placeholder="Email">
+               </div>
+               <div class="form-group col-sm-6">
+                  <label for="myPassword">Password</label>
+                  <input type="password" name="password" class="form-control" id="myPassword" placeholder="Password">
+               </div>
+            </div>
+            <div class="form-group">
+               <label for="inputAddress">Address</label>
+               <input type="text" class="address"class="form-control" id="myAddress" placeholder="1234 Main St">
+            </div>
+            <div class="form-group">
+               <label for="inputAddress2">Address 2</label>
+               <input type="text" name="address2" class="form-control" id="myAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-row">
+               <div class="form-group col-sm-6">
+                  <label for="myCity">ngày sinh</label>
+                  <input type="text" name="ngaysinh" class="form-control" id="myCity">
+               </div>
+               <div class="form-group col-sm-6">
+                  <label for="myphone">số điện thoại</label>
+                  <input type="text" name="sdt" class="form-control" id="myphone">
+               </div>
+               
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Register</button>
+         </form>
+      </div>
+</body>
+</html>dsadsadas
